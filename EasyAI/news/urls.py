3ddd2d00c4aa0a7ce18b362c92b9e81ac3974b47
fadinidERAU <fadinid@my.erau.edu>
2021-02-16 21:59:42 -0500

    New stylized webpage

--- EasyAI/news/urls.py
+++ EasyAI/news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="News" ),
+    path('', views.home, name="News"),
+    path('about/', views.about, name="About"),
 ]
--- EasyAI/news/views.py
+++ EasyAI/news/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('<h1>News Homepage</h1>')
-    
+postsDic = [
+    {
+        'author': 'admin',
+        'title': 'News Post 1',
+        'content': 'Test Post content',
+        'date_posted': 'August, 27, 2021'
+    },
+    {
+        'author': 'admin2',
+        'title': 'News Post 2',
+        'content': 'Test Post content2',
+        'date_posted': 'August, 28, 2021'
+    }
+]
 
+def home(request):
+    context = {
+        'posts': postsDic
+    }
+    return render(request, 'news/home.html', context)
+    
+def about(request):
+    return render(request, 'news/about.html', {'title': 'About'})
