051901576ef17dbb440ffec1ad3ef1cf1a0e3e87
fadinidERAU <fadinid@my.erau.edu>
2021-02-24 17:52:48 -0500

    Overhaulled User profile page
    -Change Username
    -Change Email
    -Change Picture
    
    -Pictures on posts

--- EasyAI/users/views.py
+++ EasyAI/users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+
+
+
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -16,5 +20,23 @@ def register(request):
 
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if u_fo.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+            
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
     
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html', context)
