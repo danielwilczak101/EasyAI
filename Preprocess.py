69587ae473d95282a0f609086f426d739dfb1f94
Liam Kehoe <47166826+liamtkehoe@users.noreply.github.com>
2021-09-22 14:46:34 -0400

    Update Preprocess.py
    Working file, needs to have option to change the image file. Also need to figure out how to remove axis on user image, similar to preprocessed image

--- Preprocess.py
+++ Preprocess.py
@@ -1,21 +1,29 @@
 from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-class Preprocess:
-    """Class to preprocess user images"""
+opt_size = [28, 28]
 
-    def __init__(self, filename):
-        self.filename = filename
+#Check image and saves as variable
+try:
+    #Saves preset image on computer as the user image
+    user_img = Image.open('/Users/liamkehoe/Downloads/brainfeeder_beeple-vjclips/hello.jpg')
+except IOError:
+    pass
 
-    def user_img(filename):
-        try:
-            user_img = Image.open(filename)
-        except IOError:
-            pass
+#Checks to see if image is correct size, and if not, shrinks it down to the correct size
+if user_img.size[0] == opt_size[0] & user_img.size[1] == opt_size[1]:
+    print('Image is already 28 x 28')
+else:
+    processed_img = user_img.resize([28, 28])
 
-    def get_size(filename):
-        with Image.open(filename) as image:
-            width, height = image.size
+#Create the plot to display the images
+plt.figure()
 
-    def resize(filename):
-        if filename.get_size(filename) != 28:
-            """resize imgage width here"""
+f, axarr = plt.subplots(2,1)
+
+axarr[0].imshow(user_img)
+axarr[1].imshow(processed_img)
+
+plt.axis('off')
+plt.show()
